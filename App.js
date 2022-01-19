c8642a902f12a3a46ac557306f3ad36d1b109c77
fais1242 <97963484+fais1242@users.noreply.github.com>
2022-01-19 15:51:39 +0700

    v1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React from 'react';
 const App = () => {
   return(
     <View style={styles.container}>
-      <Text style = {styles.text}>Hello fais</Text>
+      <Text style = {styles.text}>Hello World</Text>
     </View>
   )
 }
@@ -13,7 +13,8 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'gold'
   },
   text:{
     color: 'blue',
