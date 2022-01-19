7e2528b4fdcf07c2a2e66dc895dea500c8e89ad7
watsharate <76440039+watsharate@users.noreply.github.com>
2022-01-19 14:08:15 +0700

    change

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const App = () => {
     <View style={{flex:1,
                   justifyContent: 'center',
                   alignItems: 'center'}}>
-      <Text>Hello dddddddd</Text>
+      <Text>Hello ddddd</Text>
     </View>
   )
 }
