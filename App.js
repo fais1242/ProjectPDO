5b43ea5e2c3e4072174fb9120d58a92ac596bcd2
fais1242 <97963484+fais1242@users.noreply.github.com>
2022-01-19 23:23:12 +0700

    chang app

--- App.js
+++ App.js
@@ -1,49 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import login from './Srceen/login'
-const Stack = createNativeStackNavigator();
+import { View, Text } from 'react-native';
 
-function MyStack() {
+function HomeScreen() {
   return (
-    <Stack.Navigation
-    screenOption={{
-      headerStyle:{
-        backgroundColor : '#0085E6'
-      },
-      headerTintColor :'#fff',
-      headerTintStyle : {
-        fontWeight : 'bold'
-      }
-    }}
-    >
-      <Stack.Screen
-      name = "Login"
-      component={login}
-      options={{title:'Login'}}
-      />
-      
-    </Stack.Navigation>
-  )
-  
-}
-
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'gold'
-  },
-  text:{
-    color: 'blue',
-    fontSize: 30
-  }
-})
-export default function App(){
-  return(
-    <NavigationContainer></NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
