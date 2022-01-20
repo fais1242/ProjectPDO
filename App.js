0443099b7174a66d66d8df7890dae45f5ee512c6
fais1242 <97963484+fais1242@users.noreply.github.com>
2022-01-20 18:23:06 +0700

    chang main

--- App.js
+++ App.js
@@ -1,40 +1,40 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
-
 import { Button, Image } from 'react-native-elements';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Scanner from './Screen/Scanner';
 import ProductionOrder from './Screen/ProductionOrder';
 import History from './Screen/History';
+import Login from './Screen/Login';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={ styles.container }>
+
     <ScrollView>
 
-    <Text style={styles.stext}>
-        Home Screen
+      <View>
+      <Text style={styles.stext}>
+        Home
         </Text>
 
-      <Image
+        <Image
       source={{uri:'https://icon-library.com/images/users-icon-png/users-icon-png-17.jpg'}}
       style={{width:100,height:100}}
       containerStyle={{marginLeft:'auto', marginRight:'auto'}}
-
-
       />
+
+      </View>
 
     <Button
         title= "Scan QR code"
         onPress={() => navigation.navigate('Scanner')}
-        containerStyle = {{marginVertical: 10}}
-        
+        containerStyle = {{marginVertical: 10}}   
       />
 
        <Button
-        title="Production order"
+        title="Production Order"
         onPress={() => navigation.navigate('Production')}
         containerStyle = {{marginVertical: 10}}
       />
@@ -42,8 +42,12 @@ function HomeScreen({ navigation }) {
       <Button
         title="History"
         onPress={() => navigation.navigate('History')}
-        containerStyle = {{marginVertical: 10}}
-        
+        containerStyle = {{marginVertical: 10}} 
+      />
+       <Button
+        title="Login"
+        onPress={() => Login}
+        containerStyle = {{marginVertical: 10}} 
       />
 
     </ScrollView>
@@ -62,12 +66,13 @@ const styles = StyleSheet.create ({
     backgroundColor: 'lightblue'
   },
   stext:{
-    color: 'blue',
+    color: 'black',
     fontWeight: 'bold',
     fontSize: 30,
+    marginBottom:20,
+    marginLeft:'auto', marginRight:'auto'
   },
   button:{
-
   }
 });
 
@@ -75,6 +80,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Scanner" component={Scanner}  />
         <Stack.Screen name="Production" component={ProductionOrder}/>
---
+++ Screen/Login.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+function Login() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>History</Text>
+      </View>
+    );
+  }
+
+export default  Login
