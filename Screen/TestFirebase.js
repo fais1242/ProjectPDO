e780e1267cbeb2a5d1c5037169fb2557ab6c287a
fais1242 <97963484+fais1242@users.noreply.github.com>
2022-02-17 10:32:28 +0700

    17/02 v1

--- Screen/TestFirebase.js
+++ Screen/TestFirebase.js
@@ -6,10 +6,31 @@ import firestore from '@react-native-firebase/firestore';
 
 
 
-const TestFirebase = async (navigation)  => {
+const TestFirebase =  (navigation)  => {
 
-const user = await firestore().collection('Users').doc('MnfAU1GXyBxxq5tNWM5L').get()
+const user =  firestore().collection('Users').doc('S26dG3O2cMzNND1lclW6').get()
 console.log(user);
+
+firestore()
+  .collection('Users')
+  .get()
+  .then(querySnapshot => {
+    console.log('Total users: ', querySnapshot.size);
+
+    querySnapshot.forEach(documentSnapshot => {
+      console.log('User ID: ', documentSnapshot.data());
+    });
+  });
+
+// firestore()
+//   .collection('Users')
+//   .add({
+//     name: 'Ada Lovelace',
+//     age: 30,
+//   })
+//   .then(() => {
+//     console.log('User added!');
+//   });
 
 
   return (
